3968c03817f662f08fb72db9daa280a2d969c32b
Pedro Barão <phbarao@gmail.com>
2020-11-29 00:09:37 -0300

    studying

--- react-redux-demo/src/App.js
+++ react-redux-demo/src/App.js
@@ -2,12 +2,21 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import CakeContainer from './components/CakeContainer';
+import NewCakeContainer from './components/NewCakeContainer';
+import HooksCakeContainer from './components/HooksCakeContainer';
+import IceCreamContainer from './components/IceCreamContainer';
+import ItemContainer from './components/ItemContainer';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <ItemContainer cake />
+        <ItemContainer />
+        <HooksCakeContainer />
         <CakeContainer />
+        <NewCakeContainer />
+        <IceCreamContainer />
       </div>
     </Provider>
   );
--- react-redux-demo/src/components/NewCakeContainer.js
+++ react-redux-demo/src/components/NewCakeContainer.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { buyCake } from '../redux';
 
 function CakeContainer(props) {
+  const [number, setNumber] = useState(1);
   return (
     <div>
       <h2>Number of Cakes: {props.numOfCakes}</h2>
-
-      <button onClick={props.buyCake}>Buy Cake</button>
+      <input
+        type="text"
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}
+      />
+      <button onClick={() => props.buyCake(number)}>
+        Buy {number} Cake(s)
+      </button>
     </div>
   );
 }
@@ -20,7 +27,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
   return {
-    buyCake: () => dispatch(buyCake()),
+    buyCake: (number) => dispatch(buyCake(number)),
   };
 };
 
